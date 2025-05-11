68e9dfc8cf233078bf53d2571e7078ed60d10fd2
Abiram-k <abiramk0107@gmail.com>
2025-05-11 20:30:39 +0530

    fix: warning issue during build - Frontend

--- Frontend/src/protected/ProtectLogin.tsx
+++ Frontend/src/protected/ProtectLogin.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { toast } from "sonner";
-
 const ProtectLogin = ({ children }: { children: React.ReactNode }) => {
   const token = localStorage.getItem("accessToken");
   const navigate = useNavigate();
